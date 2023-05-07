b32c00399315bf8d2b0a771c816c2adc494a33fa
QianCheng <1597284782@qq.com>
2023-05-07 21:58:40 +0800

    feat: 升级依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
+// 为打包后的文件提供传统浏览器兼容性支持。
 import legacy from '@vitejs/plugin-legacy'
 // 自动按需导入 ElementPlus
 // https://element-plus.gitee.io/zh-CN/guide/quickstart.html#%E6%8C%89%E9%9C%80%E5%AF%BC%E5%85%A5
